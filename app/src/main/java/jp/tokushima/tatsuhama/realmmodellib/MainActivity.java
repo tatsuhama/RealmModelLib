53f72127e8e3345714f6160d7ce5ef1c3b54457b
tatsuhama <kenichi.tatsuhama@gmail.com>
2017-09-09 23:59:12 +0900

    Realm に保存して、読み込んで値をチェックして、トーストを表示
    生成した Book と、保存＆読み込みした Book の値が同じであれば、 `isSuccess = true` と表示
    生成した Book と、保存＆読み込みした Book の値が異なれば、 `isSuccess = false` と表示

--- app/src/main/java/jp/tokushima/tatsuhama/realmmodellib/MainActivity.java
+++ app/src/main/java/jp/tokushima/tatsuhama/realmmodellib/MainActivity.java
@@ -1,7 +1,11 @@
 package jp.tokushima.tatsuhama.realmmodellib;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import io.realm.Realm;
+import jp.tokushima.tatsuhama.realmmodellib.model.Book;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        long currentTime = System.currentTimeMillis();
+        String randomString = Long.toString(currentTime);
+        int randomInt = (int) currentTime;
+
+        Book book = createBook(randomString, randomInt);
+        saveRealm(book);
+        boolean isSuccess = checkRealmValue(randomString, randomInt);
+        Toast.makeText(this, "isSuccess = " + isSuccess, Toast.LENGTH_LONG).show();
+    }
+
+    private Book createBook(String name, int price) {
+        Book book = new Book();
+        book.name = name;
+        book.price = price;
+        return book;
+    }
+
+    private void saveRealm(Book book) {
+        Realm realm = Realm.getDefaultInstance();
+        realm.beginTransaction();
+        realm.copyToRealm(book);
+        realm.commitTransaction();
+        realm.close();
+    }
+
+    private boolean checkRealmValue(String name, int price) {
+        Realm realm = Realm.getDefaultInstance();
+        Book book = realm.where(Book.class).equalTo("name", name).findAll().first();
+        boolean result = name.equals(book.name) && price == book.price;
+        realm.close();
+        return result;
     }
 }
---
+++ app/src/main/java/jp/tokushima/tatsuhama/realmmodellib/model/Book.java
@@ -0,0 +1,8 @@
+package jp.tokushima.tatsuhama.realmmodellib.model;
+
+import io.realm.RealmObject;
+
+public class Book extends RealmObject {
+    public String name;
+    public int price;
+}
