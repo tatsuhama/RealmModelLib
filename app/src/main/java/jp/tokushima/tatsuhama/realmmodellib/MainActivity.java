2cc7708c5869e79f1981f354d328926d93812176
tatsuhama <kenichi.tatsuhama@gmail.com>
2017-09-10 00:13:25 +0900

    【復旧】Realm の保存前後でプロパティの値が変わっていた問題の修正
    realm プラグインが適用されていない app モジュールからは、RealmObject のフィールドに直接アクセスしない

--- app/src/main/java/jp/tokushima/tatsuhama/realmmodellib/MainActivity.java
+++ app/src/main/java/jp/tokushima/tatsuhama/realmmodellib/MainActivity.java
@@ -26,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Book createBook(String name, int price) {
         Book book = new Book();
-        book.name = name;
-        book.price = price;
+        book.setName(name);
+        book.setPrice(price);
         return book;
     }
 
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean checkRealmValue(String name, int price) {
         Realm realm = Realm.getDefaultInstance();
         Book book = realm.where(Book.class).equalTo("name", name).findAll().first();
-        boolean result = name.equals(book.name) && price == book.price;
+        boolean result = name.equals(book.getName()) && price == book.getPrice();
         realm.close();
         return result;
     }
--- realmmodel/src/main/java/jp/tokushima/tatsuhama/realmmodellib/model/Book.java
+++ realmmodel/src/main/java/jp/tokushima/tatsuhama/realmmodellib/model/Book.java
@@ -3,6 +3,22 @@ package jp.tokushima.tatsuhama.realmmodellib.model;
 import io.realm.RealmObject;
 
 public class Book extends RealmObject {
-    public String name;
-    public int price;
+    private String name;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
